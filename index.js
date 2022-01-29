49e8ec0ed1cc90e8abef3f283d55f2318f4f36b7
isaacmaruyama <isaacmaruyama1@gmail.com>
2022-01-29 16:42:20 -0600

    added reverse feature

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-let wordList = ["decade", "florid", "deride", "arcane", "arcade", "floral", "income", "alpaca"];
+let wordList = ["decade", "florid", "deride", "arcane", "arcade", "floral", "income", "alpaca", "paired", "parade", "scalds"];
 let word = wordList[Math.floor(Math.random() * wordList.length)].toUpperCase();
 let wordChar = word.split("");
 let wordState = [[wordChar[0], wordChar[1]].join(""), [wordChar[2], wordChar[3]].join(""), [wordChar[4], wordChar[5]].join("")];
-
-simplemaps_usmap_mapdata.state_specific['GA'].id='red';
-
 
 let nxtBox = 1;
 function checkInput() {
@@ -51,11 +48,16 @@ function checkInput() {
 
 let inputStates = [];
 let inputWord = []
+let inputReverse = false;
 simplemaps_usmap.hooks.click_state = function(id){
     if (inputWord.length<6 && inputStates.length<3) {
         inputStates.push(id);
         let inputChar = id.split("");
-        inputWord.push(inputChar[0], inputChar[1]);
+        if (inputReverse == false) {
+            inputWord.push(inputChar[0], inputChar[1]);
+        } else {
+            inputWord.push(inputChar[1], inputChar[0]);
+        }
         logInput(); 
     } 
 }
@@ -75,6 +77,29 @@ window.addEventListener("keyup", function(event) {
         inputStates = inputStates.slice(0, -1);
         logInput();
     }
+})
+
+window.addEventListener("keyup", function(event) {
+    if (event.key === "r" && inputReverse === false) {
+        inputReverse = true;
+        simplemaps_usmap_mapdata.main_settings.state_color = "#1a1a1b";
+        simplemaps_usmap_mapdata.main_settings.label_color = "#ffffff";
+        for (let i in simplemaps_usmap_mapdata.labels) {
+            let labelChar = i.split("");
+            let labelReverseChar = [];
+            labelReverseChar.push(labelChar[1], labelChar[0])
+            let labelReverse = labelReverseChar.join("");
+            simplemaps_usmap_mapdata.labels[i].name = labelReverse;
+        }
+    } else if (event.key === "r") {
+        inputReverse = false;
+        simplemaps_usmap_mapdata.main_settings.state_color = "#d3d6da";
+        simplemaps_usmap_mapdata.main_settings.label_color = "#1a1a1b";
+        for (let i in simplemaps_usmap_mapdata.labels) {
+            simplemaps_usmap_mapdata.labels[i].name = simplemaps_usmap_mapdata.labels[i].parent_id;
+        }
+    }
+    simplemaps_usmap.refresh();
 })
 
 window.addEventListener("keyup", function(event) {
